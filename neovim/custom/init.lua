2d80a0e29a72761740de1d30ed48ee744aaa5cf6
Shivam Shandilya <shiv.05cs@gmail.com>
2024-06-24 15:26:08 +0530

    feat(nvim): set relative line numbering in normal mode and absolute line numbering in insert mode

--- neovim/custom/init.lua
+++ neovim/custom/init.lua
@@ -1,2 +1,11 @@
-vim.opt.relativenumber = true
 vim.g.mapleader = ","
+
+-- Create an augroup to manage the autocommands
+-- Enable relative line numbering in normal mode and absolute line numbering in insert mode
+vim.cmd([[
+augroup numbertoggle
+  autocmd!
+  autocmd BufEnter,FocusGained,InsertLeave,WinEnter * if &nu && mode() != "i" | set relativenumber | endif
+  autocmd BufLeave,FocusLost,InsertEnter,WinLeave * if &nu | set norelativenumber | endif
+augroup END
+]])
